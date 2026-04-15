98892afcc2390dfe2a23a08703528d815f4274d9
PlinyTheYounger0 <PlinyTheYounger0@proton.me>
2026-04-15 08:16:46 -0700

    Created startRepl(). Moved REPL logic from main() to startRepl().

--- repl.go
+++ repl.go
@@ -1,8 +1,41 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		scanned := scanner.Scan()
+		if scanned == false {
+			fmt.Printf("Error scanning: %v", scanner.Err())
+		}
+
+		text := scanner.Text()
+		input := cleanInput(text)
+		userCommand := input[0]
+
+		commandRegistry := getCommands()
+
+		command, exists := commandRegistry[userCommand]
+		if !exists {
+			fmt.Println("Unknown Command")
+		} else {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+
+	}
+}
 
 func cleanInput(text string) []string {
 	inputLower := strings.ToLower(text)
