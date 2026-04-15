8d1695be11c8c7696c1a0ecdfb5612245482e4f7
PlinyTheYounger0 <PlinyTheYounger0@proton.me>
2026-04-15 06:13:54 -0700

    Added REPL logic to func main()

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		scanned := scanner.Scan()
+		if scanned == false {
+			fmt.Printf("Error scanning: %v", scanner.Err())
+		}
+
+		text := scanner.Text()
+		input := cleanInput(text)
+
+		fmt.Printf("Your first word was: %s\n", input[0])
+	}
 }
